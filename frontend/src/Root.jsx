8e76aa88dac186116498c488de8dce866a492615
siascone <spencer.iascone@gmail.com>
2023-08-10 13:07:27 -0600

    Setup react frontend and store

---
+++ frontend/src/Root.jsx
@@ -0,0 +1,22 @@
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import configureStore from './store/store.js'
+
+const store = configureStore();
+
+if (import.meta.env.NODE_ENV !== 'production') {
+    window.store = store;
+}
+
+function Root() {
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    );
+}
+
+export default Root;
