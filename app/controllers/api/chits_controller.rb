ceab4682c382e51c427653d38ad21a49e947a8ff
siascone <spencer.iascone@gmail.com>
2023-08-11 16:03:06 -0600

    Add chit routes and controller actions, needs testing

---
+++ app/controllers/api/chits_controller.rb
@@ -0,0 +1,47 @@
+class Api::ChitsController < ApplicationController
+
+    wrap_parameters include: Chit.attribute_names
+    
+    def show 
+        @chit = Chit.find_by(id: params[:id])
+        render json: @chit
+    end
+
+    def index
+        @chits = Chit.all
+        render json: @chits
+    end
+
+    def create
+        @chit = Chit.new(chit_params)
+        @chit.user_id = current_user.id
+
+        if @chit.save
+            render json: @chit
+        else
+            render json: @chit.errors.full_messages
+        end
+    end
+
+    def update
+        @chit = Chit.find_by(id: params[:id])
+
+        if @chit && @chit.update(chit_params)
+            render json: @chit
+        else
+            render json: @chit.errors.full_messages
+        end
+    end
+
+    def destroy
+        @chit = Chit.find_by(id: params[:id])
+        @chit.destroy
+        render json: @chit
+    end
+
+    private
+
+    def chit_params
+        params.require(:chit).permit(:body)
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
+    resources :chits, only: [:show, :index, :create, :update, :destroy]
     resource :session, only: [:show, :create, :destroy]
   end
 end
